1e61b09ad2c391e952bc73afe3774f12186e6b6d
yatinburhmi <yatinburhmi@gmail.com>
2022-08-02 10:43:31 -0400

    Resolved Bug: createpost not working

--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -14,7 +14,7 @@ export const getPosts = async (req, res) => {
 }
 
 export const createPost = async (req, res) => {
-    const body = req.body;
+    const post = req.body;
 
     //creating a new post and passing it in schema
     const newPost =  new PostMessage(post);
